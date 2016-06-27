8c79e01f3a99ca9d74ce4ba7b24c6d4c4da3ce7c
zhoushuqiang <zhoushuqiang168@126.com>
2016-06-27 18:18:26 +0800

    suker:add ffmpeg compile with thirdparty libs ok

--- ubuntu-bld.sh
+++ ubuntu-bld.sh
@@ -7,15 +7,21 @@
 #http://johnvansickle.com/ffmpeg/release-source/
 #https://ffmpeg.zeranoe.com/builds/
 #http://pkgs.repoforge.org/
+#http://www.slackware.com/~alien/slackbuilds/ffmpeg/build/
 
 length=0
-SETUP_PATH=/home/suker/work/study/log/2016/06/24/ffmpegbld/build
+SETUP_PATH=`echo $PWD`/build
+echo setuppath=$SETUP_PATH
+
+OGGDIR=""
 
 bldmodule()
 {
 	echo "==================================================build module $1 begin"
 	cd $1
-	./configure --prefix=$SETUP_PATH/$1 && make && make install
+	make clean
+	echo "./configure --prefix=$SETUP_PATH/$1 $2 && make && make install"
+	./configure --prefix=$SETUP_PATH/$1 $2 && make && make install
 	cd ../
 	echo "==================================================build module $1 end"
 }
@@ -26,6 +32,7 @@ cmakemodule()
 	cd $1
 	mkdir -p bld &&
 	cd bld &&
+	make clean
 	cmake -DCMAKE_INSTALL_PREFIX=$SETUP_PATH/$1 \
           -DBUILD_SHARED_LIBS=OFF \
    		  ../$2  && make && make install
@@ -33,11 +40,11 @@ cmakemodule()
 	echo ==================================================build module $1 end
 }
 
-curlgetsrc()
+comnbld()
 {
 	localname=`echo $1 | awk -F "/" '{print $NF}'`
 	length=`expr $length + 1`
-	echo "index:$length, ========curl get source:$1"
+	echo "$PWD, index:$length, ========curl get source:$1"
 
 	curl -L  $1 -o $localname
 
@@ -50,40 +57,205 @@ curlgetsrc()
 	elif [[ $1 =~ "tar.xz" ]]; then
 #		echo "\$1 contains tar.xz"
 		tar -Jxf $localname
+	elif [[ $1 =~ "zip" ]]; then
+#		echo "\$1 contains zip"
+		unzip $localname
 	fi
 
+
+	if [[ $1 =~ "libogg" ]]; then
+		echo "\$1 contains libogg"
+		OGGDIR=$localname
+	fi
+
+	OGGDIR=libogg-1.3.2
+	echo "========================oggdir:$OGGDIR"
+	
 	dirname=`echo $localname | awk -F ".tar" '{print $1}'`
-#	echo "=================dir-name:$dirname"
-	time  bldmodule $dirname
+	echo "=================dir-name:$dirname"
+
+	if [[ $2 == 1 ]]; then
+		time  bldmodule $dirname
+	elif  [[ $2 == 2 ]]; then
+		echo "============use cmake mode"
+		time  cmakemodule $dirname
+	elif  [[ $2 == 3 ]]; then
+		echo "============libogg dependence mode"
+		time  bldmodule $dirname   --with-ogg=$SETUP_PATH/$OGGDIR
+	elif  [[ $2 == 4 ]]; then
+		echo "============vidstab compile mode"
+		time  cmakemodule georgmartius-vid.stab-3b35b4d
+	elif  [[ $2 == 5 ]]; then
+		echo "============openjpeg compile mode"
+		time  cmakemodule openjpeg-version.2.1
+	elif  [[ $2 == 6 ]]; then
+		echo "============x264 compile mode"
+		X264DIR=x264-snapshot-20160623-2245
+		time  bldmodule $X264DIR
+		mkdir -p $SETUP_PATH/$X264DIR/lib/
+		mkdir -p $SETUP_PATH/$X264DIR/include/
+		cp $X264DIR/libx264.a        $SETUP_PATH/$X264DIR/lib/
+		cp $X264DIR/x264.h           $SETUP_PATH/$X264DIR/include/
+		cp $X264DIR/x264_config.h    $SETUP_PATH/$X264DIR/include/
+	elif  [[ $2 == 7 ]]; then
+		echo "============xvidcord compile mode"
+		XVIDCOREDIR=xvidcore
+		time  bldmodule  xvidcore/build/generic
+		cp -rf $SETUP_PATH/xvidcore/generic/include  $SETUP_PATH/$XVIDCOREDIR
+		cp -rf $SETUP_PATH/xvidcore/generic/lib  $SETUP_PATH/$XVIDCOREDIR
+	elif  [[ $2 == 8 ]]; then
+		ZIMGDIR=zimg-master
+		echo "============$ZIMGDIR compile mode"
+		cd $ZIMGDIR
+		./autogen.sh
+		cd ../
+		time  bldmodule  $ZIMGDIR
+	elif  [[ $2 == 9 ]]; then
+		cd $dirname
+		mkdir -p /home/suker/work/rtmp
+		sed  -i  's/prefix=\/usr\/local/prefix=\/home\/suker\/work\/rtmp/g'  Makefile
+		sed  -i  's/prefix=\/usr\/local/prefix=\/home\/suker\/work\/rtmp/g'  librtmp/Makefile
+		make  && make install
+		cd ../
+	elif  [[ $2 == 10 ]]; then
+		cd $dirname
+		./configure --prefix=$SETUP_PATH/$dirname\
+		 --enable-ffplay \
+		 --enable-gpl \
+		 --enable-version3 \
+		 --enable-static \
+		 --disable-debug \
+		 --enable-libmp3lame \
+		 --enable-libx264 \
+		 --enable-libx265 \
+		 --enable-libwebp \
+		 --enable-libspeex \
+		 --enable-libvorbis \
+		 --enable-libvpx \
+		 --enable-libfreetype \
+		 --enable-fontconfig \
+		 --enable-libxvid \
+		 --enable-libopencore-amrnb \
+		 --enable-libopencore-amrwb \
+		 --enable-libtheora \
+		 --enable-libvo-amrwbenc \
+		 --enable-gray \
+		 --enable-libopenjpeg \
+		 --enable-libopus \
+		 --enable-libass \
+		 --enable-libvidstab \
+		 --enable-libsoxr \
+		 --enable-frei0r \
+		 --enable-libfribidi \
+		 --disable-indev=sndio \
+		 --disable-outdev=sndio \
+		 --enable-librtmp \
+		 --enable-libzimg \
+		 --extra-cflags="-I$SETUP_PATH/install/include" \
+		 --extra-ldflags="-L$SETUP_PATH/install/lib -L/usr/lib/x86_64-linux-gnu/" \
+		 --extra-libs="-lexpat -lpng -lharfbuzz -lgomp -ldl  -lz -lpthread  -lstdc++ -lm -lrt -lfreetype" 
+
+		make  && make install
+		cd ../
+	fi
 }
 
 
-time  curlgetsrc  https://sourceforge.net/projects/opencore-amr/files/fdk-aac/fdk-aac-0.1.4.tar.gz
-time  curlgetsrc  https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.0.tar.bz2
-time  curlgetsrc  http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.bz2
-time  curlgetsrc  https://files.dyne.org/frei0r/releases/frei0r-plugins-1.5.0.tar.gz
-time  curlgetsrc  https://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz
-time  curlgetsrc  https://github.com/libass/libass/releases/download/0.13.2/libass-0.13.2.tar.xz
-time  curlgetsrc  https://sourceforge.net/projects/soxr/files/soxr-0.1.2-Source.tar.xz
-time  curlgetsrc  https://github.com/georgmartius/vid.stab/tarball/release-0.98b/georgmartius-vid.stab-release-0.98b-0-g3b35b4d.tar.gz
-time  curlgetsrc  http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.xz
-time  curlgetsrc  http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.xz
-time  curlgetsrc  http://downloads.xiph.org/releases/theora/libtheora-1.2.0alpha1.tar.xz
-time  curlgetsrc  https://sourceforge.net/projects/opencore-amr/files/opencore-amr/opencore-amr-0.1.3.tar.gz
-time  curlgetsrc  http://storage.googleapis.com/downloads.webmproject.org/releases/webm/libvpx-1.5.0.tar.bz2
-time  curlgetsrc  https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-0.5.0.tar.gz
-time  curlgetsrc  ftp://ftp.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
-time  curlgetsrc  https://sourceforge.net/projects/opencore-amr/files/opencore-amr/opencore-amr-0.1.3.tar.gz
-time  curlgetsrc  https://sourceforge.net/projects/opencore-amr/files/vo-aacenc/vo-aacenc-0.1.3.tar.gz
-time  curlgetsrc  https://sourceforge.net/projects/opencore-amr/files/vo-amrwbenc/vo-amrwbenc-0.1.3.tar.gz
-time  curlgetsrc  http://downloads.xiph.org/releases/opus/opus-1.1.2.tar.gz
-time  curlgetsrc  http://downloads.xiph.org/releases/speex/speexdsp-1.2rc3.tar.gz
-time  curlgetsrc  https://github.com/uclouvain/openjpeg/archive/version.2.1.tar.gz
-time  curlgetsrc  https://ffmpeg.zeranoe.com/builds/source/external_libraries/rtmpdump-20151223-git-fa8646d.tar.xz
-time  curlgetsrc  http://downloads.xvid.org/downloads/xvidcore-1.3.4.tar.bz2
-time  curlgetsrc  https://sourceforge.net/projects/zimg/files/zimg/zimg-5.0.0/zimg-5.0.0.tar.gz
-time  curlgetsrc  http://ffmpeg.org/releases/ffmpeg-3.0.2.tar.bz2
-time  ##===============================================================================
+
+cfgbld()
+{
+	comnbld $1 1
+}
+
+cmakebld()
+{
+	comnbld $1 2
+}
+
+withogg()
+{
+	comnbld $1 3
+}
+
+vidstab()
+{
+	comnbld $1 4
+}
+
+openjpeg()
+{
+	comnbld $1 5
+}
+
+x264bld()
+{
+	comnbld $1 6
+}
+
+xvidcord()
+{
+	comnbld $1 7
+}
+
+zimgbld()
+{
+	comnbld $1 8
+}
+
+rtmpbld()
+{
+	comnbld $1 9
+}
+
+x265bld()
+{
+	hg clone $1
+	DSTDIR=x265/build/linux
+	cd $DSTDIR
+	./make-Makefiles.bash
+	make
+	mkdir -p $SETUP_PATH/x265/lib/pkgconfig
+	mkdir -p $SETUP_PATH/x265/include/
+	cp ./libx265.*              $SETUP_PATH/x265/lib/
+	cp ./x265.pc                $SETUP_PATH/x265/lib/pkgconfig
+	cp ./../../source/x265.h    $SETUP_PATH/x265/include/
+	cp ./x265_config.h          $SETUP_PATH/x265/include/
+	cd ../../../
+#	make install
+}
+
+ffmbld()
+{
+	comnbld $1 10
+}
+
+
+#time  cfgbld  https://www.fribidi.org/download/fribidi-0.19.7.tar.bz2                                                               #ok
+#time  cfgbld  https://sourceforge.net/projects/opencore-amr/files/fdk-aac/fdk-aac-0.1.4.tar.gz                                      #ok
+#time  cfgbld  https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.0.tar.bz2                                     #ok
+#time  cfgbld  http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.bz2                                               #ok
+#time  cmakebld  https://files.dyne.org/frei0r/releases/frei0r-plugins-1.5.0.tar.gz                                                  #ok
+#time  cfgbld  https://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz                                              #ok
+#time  cfgbld  https://github.com/libass/libass/releases/download/0.13.2/libass-0.13.2.tar.xz                                        #error(must have fribidi)
+#time  cmakebld  https://sourceforge.net/projects/soxr/files/soxr-0.1.2-Source.tar.xz                                                #ok
+#time  vidstab https://github.com/georgmartius/vid.stab/tarball/release-0.98b/georgmartius-vid.stab-release-0.98b-0-g3b35b4d.tar.gz  #ok(error bef)
+#time  cfgbld  http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.xz                                                            #ok
+#time  withogg  http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.xz                                                     #ok
+#time  withogg  http://downloads.xiph.org/releases/theora/libtheora-1.2.0alpha1.tar.xz                                               #ok
+#time  cfgbld  https://sourceforge.net/projects/opencore-amr/files/opencore-amr/opencore-amr-0.1.3.tar.gz                            #ok
+#time  cfgbld  http://storage.googleapis.com/downloads.webmproject.org/releases/webm/libvpx-1.5.0.tar.bz2                            #ok(need clean)
+#time  cfgbld  https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-0.5.0.tar.gz                           #ok
+#time  x264bld  ftp://ftp.videolan.org/pub/x264/snapshots/last_x264.tar.bz2                                                          #ok(error bef)
+#time  cfgbld  https://sourceforge.net/projects/opencore-amr/files/vo-aacenc/vo-aacenc-0.1.3.tar.gz                                  #ok
+#time  cfgbld  https://sourceforge.net/projects/opencore-amr/files/vo-amrwbenc/vo-amrwbenc-0.1.3.tar.gz                              #ok
+#time  cfgbld  http://downloads.xiph.org/releases/opus/opus-1.1.2.tar.gz                                                             #ok
+#time  cfgbld  http://downloads.xiph.org/releases/speex/speex-1.2rc2.tar.gz                                                          #ok
+#time  openjpeg  https://github.com/uclouvain/openjpeg/archive/version.2.1.tar.gz                                                    #ok(error bef)
+#time  rtmpbld  https://ffmpeg.zeranoe.com/builds/source/external_libraries/rtmpdump-20151223-git-fa8646d.tar.xz                     #ok(error bef)
+#time  xvidcord  http://downloads.xvid.org/downloads/xvidcore-1.3.4.tar.bz2                                                          #ok(error bef)
+#time  zimgbld  https://github.com/sekrit-twc/zimg/archive/master.zip                                                                #ok(error bef)
+#time  x265bld  https://bitbucket.org/multicoreware/x265                                                                              #ok
+time  ffmbld  http://ffmpeg.org/releases/ffmpeg-3.0.2.tar.bz2
 #===============================================================================
 #===============================================================================
 #===============================================================================
@@ -155,6 +327,11 @@ time  ##========================================================================
 #http://www.videolan.org/developers/x265.html
 #hg clone http://hg.videolan.org/x265
 #https://x265.com/
+#hg clone https://bitbucket.org/multicoreware/x265
+#cd x265/build/linux
+#./make-Makefiles.bash
+#make
+#make install
 #-------------------------------------------------------------------------------
 #https://sourceforge.net/projects/opencore-amr/files/
 #https://sourceforge.net/projects/opencore-amr/files/opencore-amr/opencore-amr-0.1.3.tar.gz/download
@@ -209,12 +386,45 @@ time  ##========================================================================
 #	if [ ! -n "$" ]; then 
 
 #-------------------------------------------------------------------------------
+#time  cfgbld  https://sourceforge.net/projects/zimg/files/zimg/zimg-5.0.0/zimg-5.0.0.tar.gz                                          #error
+#https://github.com/sekrit-twc/zimg
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+#	find ./ -name Makefile | xargs grep -rn prefix=
+#	find ./ -name Makefile | sed 's/prefix=/sukersuker=/g' 
+#	echo "${SETUP_PATH}/${$1}"
+
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
+#export PKG_CONFIG=/usr/bin/pkg-config
+#export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:/home/suker/work/study/log/2016/06/24/ffmpegbld/build/install/lib/pkgconfig
+#/home/suker/work/study/log/2016/06/24/ffmpegbld/build/install/lib/pkgconfig
+#export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+
+#libass-0.13.2.tar.xz
+#checking for FRIBIDI... no
+#configure: error: Package requirements (fribidi >= 0.19.0) were not met:
+#No package 'fribidi' found
+#Consider adjusting the PKG_CONFIG_PATH environment variable if you
+#installed software in a non-standard prefix.
+#Alternatively, you may set the environment variables FRIBIDI_CFLAGS
+#and FRIBIDI_LIBS to avoid the need to call pkg-config.
+#See the pkg-config man page for more details.
+# pkg-config --list-all
+#pkg-config --modversion fribidi
+
+#-------------------------------------------------------------------------------
+
+
+#mac os
+#http://mac.softpedia.com/get/Development/Compilers/CMake.shtml#download
+#https://cmake.org/
+#/home/suker/work/study/log/2016/06/24/ffmpegbld/build
+#/Users/zhoushuqiang/Desktop/suker/source/github.com/zhoushuqiang/ffmbld
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
+#time  cfgbld  http://downloads.xiph.org/releases/speex/speexdsp-1.2rc3.tar.gz                                                       #ok
+
 #-------------------------------------------------------------------------------
